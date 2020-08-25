ddf780c26c9e58fb83c0f8e6963d18c1851332a3
Antonio Fonseca <antonio@vpn1722516811.its.yale.internal>
2020-08-24 23:18:22 -0400

    update INP_imaging_mouse.m

--- Advanced/INP_imaging_mouse.m
+++ Advanced/INP_imaging_mouse.m
@@ -2,7 +2,7 @@
 %     INP data analysis bootcamp, Yale University
 %     Imaging Section: Mouse Data
 %
-%     8/27/2019 KAF
+%     8/27/2020 KAF-CJB-AF
 %     
 %     In each section, use the suggestions to fill in the variables and the
 %     rest of the necessary code.
@@ -31,39 +31,42 @@
 
 %% Let's start with the contrast data, 'sampleCRFdata.mat'. 
 
+clear
+close all
 %  Load the data here
+load('../Data/sampleCRFdata_SOMpopln.mat'); %  Use sampleCRFdata (4 neurons) or sampleCRFdata_SOMpopln (31 neurons)
+stimType = 'Contrast (%)';         %  change to 'Size (degrees)' for sampleRFdata.mat
 
 
+%% Task1
+%  Plot three random neurons in subplots. Requirements:
+%  1) Each subplot should have the number or the neuron as its title.
+%  2) Add labels for X and Y axes
+%  3) link the time axis (linkaxes)
+%  P.S.: What do you notice about these cells? 
+%  P.S2: Design a script that would allow you to plot N neurons. So please,
+%  don't just call the function "plot" three times :)
+
+numCells     =   size(cellData,2);                          % number of neurons
 
 
-stimType = 'Contrast (%)';          %  change to 'Size (degrees)' for sampleRFdata.mat
-
-%  Plot your favorite 3 neurons in subplots, link the time axis (linkaxes).  What do you
-%  notice about these cells? 
-
-
-
-
-%  Now let's plot a neuron with the times of the visual stimulus as '*'
-%  using scatter.  
+%% Task2
+%  Plot a neuron calcium trace and mark the time stamps of the visual
+%  stimulus. Requirement:
+%  1) Add title, labels to the axes and legend
+%  2) Whatever type of marker/line you decide to use, make sure it has
+%  distintc colors for visOn and visOff.
 
 nrnNum       = 4;                         % pick your favorite neuron to plot first
-visOnT       =                            % use the vector 'time' to find the time of your visual stim onset  
-visOffT      =                            % the time of your visual stim offset    
-
-
-figure; 
-plot(    )                                % plot the cell activity
-hold on; 
-scatter(    , 10*ones(size(visOnT)),'*g');  %  plot onset times in green
-scatter(    , 10*ones(size(visOffT)),'*r');  %  plot offset times in red
+visOnT       = time(visOn);               % use the vector 'time' to find the time of your visual stim onset  
+visOffT      = time(visOff);              % the time of your visual stim offset    
 
 
 %%  Let's make our heatmap of neuron activity over each visual stimulation trial, with average activity at bottom
 
 % first set some parameters
 
-numCells     =                            % number of neurons
+numCells     =   size(cellData,2);                          % number of neurons
 
 % Let's choose a time window around the visual stimulus onset so that we can
 % align our traces.   We'll start with this:
@@ -117,34 +120,44 @@ caxis([0 3])                        % color axis limits
 hold on;
 plot(   ,    ,'--w','linew',2)      % mark visual stimulation onset with a white dashed line
 title(['Neuron ', ? ])              % add the neuron number to your title using 'neuronNum'
-ylabel('Trial','fontsize',12)
+ylabel('Trial','fontsize',15)
 
-%% find the average response to stimuli over every trial.  Check that you're averaging correctly by confirming the size is right
-meanResp = 
+%% Task 3 
+% find the average response to stimuli over every trial.  Check that you're averaging correctly by confirming the size is right
+meanResp = ?
 
 %  plot the average response in the the bottom subplot
 subplot(5,1,5)
 plot(timeTrial,meanResp,'k')
-xlabel('Time (s)', 'fontsize',12)
-ylabel({'Average ' ; '\Delta F/F_0'}, 'fontsize',12)
+xlabel('Time (s)')
+ylabel({'Average ' ; '\Delta F/F_0'})
 xlim([timeTrial(1), timeTrial(end)])
+set(gca,'FontSize',15)
 
-outputFigName = ['Neuron_' ? '_heatmap'];                       % fill in '?' for the correct neuron number.   
+outputFigName = ['Neuron_' , num2str(nrnNum),'_heatmap'];                       % fill in '?' for the correct neuron number.   
 
 % Save the visResp data for each cell.  Make sure it is saving in your
-% desired folder (i.e. specify your path)
-
+% desired folder (i.e. specify your path). Obs: create a new folder t osave
+% your .mat and plots. Add a command to check if the folder already exist
+% before creating the folder.
+if ~exist('../plots', 'dir')
+   mkdir('../plots')
+end
+save(['../plots/Neuron_',num2str(nrnNum)], 'visResp'); 
 
 % Save the plots as .fig, .jpg, .eps.  For .jpg , .eps, open them to make
 % sure they've saved as you intended.  
-
-
+fulloutputFn = ?   % Full path to save the image 
+savefig(fulloutputFn)
+saveas(gcf, fulloutputFn, 'epsc')
+saveas(gcf, fulloutputFn, 'jpeg')
 
 %% How long after visual stim onset does the average activity (meanResp) peak?  
+[~,mxIdx] = max(meanResp); 
+pkTime = timeTrial(mxIdx); 
 
 
-
-%% Exercises for the heatmap %% 
+%% Extra exercises for the heatmap (optional) %% 
 
 % (1) Change the window sizes.  Look at two cycles simultaneously. 
 % (2) Change the proportion of subplots... 
@@ -156,7 +169,8 @@ outputFigName = ['Neuron_' ? '_heatmap'];                       % fill in '?' fo
 % (6) Make the same plots for our "sampleRFdata".
 
 
-%% Now we will look at whether the cells respond differently to various contrasts/sizes
+%% Task 4
+%  Now we will look at whether the cells respond differently to various contrasts/sizes
 %  We will fill a vResp matrix (as our earlier visResp), but this time only
 %  use values between visOn and visOff
 
@@ -175,29 +189,39 @@ vRespAvg =
 
 stimAxis =                  % Find the unique stim values. 
 axLen    =                  % What is the length of stimAxis?
+
     
-% now we will create vectors for the mean and standard deviation of visual
+% now we will create vectors for the mean and SEM of visual
 % stim values across every distinct presentation type
 
 % initialize
 meanData = 
-stdData = 
+semData =  
 
-% find data for each distinct visual stim, average and take standard deviation
+% find data for each distinct visual stim, average and take the SEM
 
-for i =                  % iterate through all possible visual stim values       
-    vdata           =                   % what is the vRespAvg for this iteration of stim value ?
-    meanData(i) = 
-    stdData(i)  = 
+for istim =          % iterate through all possible visual stim values       
+    vdata       =                  % what is the vRespAvg for this iteration of stim value ?
+    meanData(istim) =   
+    semData(istim)  =  
 end
     
         
-% error bar plot of mean and standard deviation over observations within each feature
+% error bar plot of mean and SEM over observations within each feature
 f = figure; 
 errorbar(  , ,  , 's')                              
-title(['Error bar plot for Neuron ', ? ])           % replace ?
-xlabel(stimType, 'fontsize',12)
+title(['Error bar plot for Neuron ', ? ])            % replace ?
+xlabel(stimType)
+ylabel('Response')
+set(gca,'FontSize',15)
 
+
+
+%% Task 5. Design a spike detector
+%% Now things will get interesting. We want you to design a detector for the neuron's response to the stimulus. 
+%% As a proxy for the GT, we will use the time stamps of visOn.
+%% There are many ways to achieve this. Use your creativity. At the end of the bootcamp, 
+%% we will check who evaluate who obtained the best recall rate.
 
 %% Exercises
 
@@ -220,16 +244,3 @@ xlabel(stimType, 'fontsize',12)
 % (3) Plot the average across all cells.
 % (4) Make a new plotting function 'makeHeatmap' to make your
 %       heatmap/average trace figure.  
-
-
-
-%% If you want more of a challenge, ask about the wheel data ! 
-
-
-
-
-
-
-
-
-
